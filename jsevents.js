661d0009e29d0d17cb86deb5846a094ee063f196
Arnab <arnabbasak11@gmail.com>
2019-08-10 10:51:01 +0530

    commiting javascript codes

---
+++ jsevents.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+<script type="text/javascript">
+function sayHello() {
+ document.write ("Hello World")
+}
+</script>
+14. EVENTS 
+Javascript
+75
+</head>
+<body>
+<p> Click the following button and see result</p>
+<input type="button" onclick="sayHello()" value="Say Hello" />
+</body>
+</html>
